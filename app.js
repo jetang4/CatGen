670366d49609146ce144b60da959c5d08471c4bc
Kristie Desiree <kly5@mail.sfsu.edu>
2018-02-20 17:17:24 -0800

    move app.js into CatGen folder from Source folder

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+
+//make all images and JS and CSS files accessible
+app.use(express.static(__dirname + '/Source')); //dirname refers to 'CatGen'
+app.use(express.static(__dirname + '/Assets'));
+
+
+//homepage
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/Source/index.html');
+});
+
+//about
+app.get('/about', function(req, res) {
+    res.sendFile(__dirname + '/Source/aboutUs.html');
+});
+
+//lessons of mendelian genetics
+app.get('/mendeliangenetics_heredity', function(req, res) {
+    res.sendFile(__dirname + '/Source/mendeliangenetics_heredity.html');
+});
+
+app.get('/mendeliangenetics_dominanceAndRecessiveness', function(req, res) {
+    res.sendFile(__dirname + '/Source/mendeliangenetics_dominanceAndRecessiveness.html');
+});
+
+app.get('/mendeliangenetics_probability', function(req, res) {
+    res.sendFile(__dirname + '/Source/mendeliangenetics_probability.html');
+});
+
+app.get('/mendeliangenetics_punnettSquares', function(req, res) {
+    res.sendFile(__dirname + '/Source/mendeliangenetics_punnettSquares.html');
+});
+
+//lessons of catgen
+app.get('/catgen_geneinfo', function(req, res) {
+    res.sendFile(__dirname + '/Source/catgen_geneinfo.html');
+});
+
+
+app.get('/CATGEN', function(req, res) {
+    res.sendFile(__dirname + '/Source/CATGEN.html');
+});
+
+
+app.get('/quiz', function(req, res) {
+    res.sendFile(__dirname + '/Source/quiz.html');
+});
+
+app.get('/homepage', function(req, res) {
+    res.sendFile(__dirname + '/Source/homepage.html');
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))

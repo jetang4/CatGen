3fe5a8b3a970298afe6866a4afad110f3ae78b9e
Kristie Desiree <kly5@mail.sfsu.edu>
2018-02-22 14:26:18 -0800

    remove JS code that was used for single-page layout. slight modification of headers etc

--- Source/script.js
+++ Source/script.js
@@ -1,11 +1,3 @@
-function validateForm() {
-    var x = document.forms["myForm"]["dominantAllele"].value;
-    if (x == "DD") {
-        alert("Name must be filled out");
-        return false;
-    }
-}
-
 function quizValidator() {
     var x, text;
 
@@ -21,16 +13,3 @@ function quizValidator() {
     document.getElementById("result").innerHTML = text;
 }
 
-//JAVASCRIPT CODE TO HIDE/DISPLAY PAGES WHEN NAVIGATION BAR IS CLICKED
-function show(elementID) {
-    var ele = document.getElementById(elementID);
-    if (!ele) {
-        alert("no such element");
-        return;
-    }
-    var pages = document.getElementsByClassName('page');
-    for(var i = 0; i < pages.length; i++) {
-        pages[i].style.display = 'none';
-    }
-    ele.style.display = 'block';
-}

535f06fb882c824a66ec8fad2d7f66daef50b63e
Kristie Desiree <kly5@mail.sfsu.edu>
2018-03-06 12:45:43 -0800

    modify the quiz function to be more flexible

--- Source/script.js
+++ Source/script.js
@@ -1,11 +1,11 @@
-function quizValidator() {
+function quizValidator(correctAnswer) {
     var x, text;
 
     // Get the value of the input field with id="numb"
     x = document.getElementById("domrec").value;
 
     // If x is Not a Number or less than one or greater than 10
-    if (x != "ff") {
+    if (x != correctAnswer) {
         text = "Try again";
     } else {
         text = "Correct";

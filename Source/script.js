092cfa1400922bbef6f006963b8e0eb336f5211d
Kristie Desiree <kly5@mail.sfsu.edu>
2018-03-08 15:04:53 -0800

    revert back to regular navigation bar

--- Source/script.js
+++ Source/script.js
@@ -1,15 +1,17 @@
-function quizValidator(correctAnswer) {
+function quizValidator(correctAnswer, inputBox) {
     var x, text;
-
+    console.log(inputBox);
     // Get the value of the input field with id="numb"
-    x = document.getElementById("domrec").value;
+    x = document.getElementById(inputBox).value;
 
     // If x is Not a Number or less than one or greater than 10
-    if (x != correctAnswer) {
+    if (x !== correctAnswer) {
         text = "Try again";
     } else {
         text = "Correct";
     }
-    document.getElementById("domrec_result").innerHTML = text;
+    var inputResult = inputBox + "-result";
+    document.getElementById(inputResult).innerHTML = text;
+    console.log(document.getElementById(inputResult));
 }
 
